afc9c90d07d4be06423379ba06ed219c69e886ca
Rui Gonçalves <ruippeixotog@gmail.com>
2015-05-31 00:54:55 +0100

    Update C version to comply with the C89 standard

--- c/quicksort.c
+++ c/quicksort.c
@@ -8,8 +8,8 @@ void swap(int* arr, int i, int j) {
 void quicksort(int* arr, int st, int end) {
   if(st == end) return;
 
-  int sep = st;
-  for(int i = st + 1; i < end; i++) {
+  int i, sep = st;
+  for(i = st + 1; i < end; i++) {
     if(arr[i] < arr[st]) swap(arr, ++sep, i);
   }
 
@@ -22,12 +22,13 @@ int main() {
   int n; scanf("%d", &n);
   int* arr = malloc(n * sizeof(int));
 
-  for(int i = 0; i < n; i++)
+  int i;
+  for(i = 0; i < n; i++)
     scanf("%d", &arr[i]);
 
   quicksort(arr, 0, n);
 
-  for(int i = 0; i < n; i++)
+  for(i = 0; i < n; i++)
     printf(i == 0 ? "%d" : " %d", arr[i]);
   printf("\n");
 
