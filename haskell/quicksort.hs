6a12ecb282a7969e978495823761279c4d5ab6e7
Rui Gonçalves <ruippeixotog@gmail.com>
2015-05-31 01:28:18 +0100

    Add Haskell implementation

---
+++ haskell/quicksort.hs
@@ -0,0 +1,18 @@
+import Data.List
+
+quicksort :: [Int] -> [Int]
+quicksort [] = []
+quicksort (pivot : xs) =
+  let (lower, upper) = partition (< pivot) xs
+  in quicksort lower ++ [pivot] ++ quicksort upper
+
+readIntList :: String -> [Int]
+readIntList = (map read) . words
+
+showIntList :: [Int] -> String
+showIntList = (intercalate " ") . (map show)
+
+main = do n <- getLine
+          arr <- getLine
+          let sorted = quicksort (readIntList arr)
+          putStrLn (showIntList sorted)
