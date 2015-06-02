4850e22069c60d49aeb8983b33f85ccac2d09d77
Rui Gonçalves <ruippeixotog@gmail.com>
2015-06-03 00:00:11 +0100

    Add Racket implementation

---
+++ racket/quicksort.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+(define (quicksort xs)
+  (if (null? xs) null
+      (let*-values ([(pivot) (car xs)]
+                    [(lower upper) (partition (curry >= pivot) (cdr xs))])
+        (append (quicksort lower) (list pivot) (quicksort upper)))))
+
+(define n (read))
+(define arr (for/list ([i (in-range n)]) (read)))
+
+(define sorted (quicksort arr))
+(displayln (string-join (map number->string sorted)))
