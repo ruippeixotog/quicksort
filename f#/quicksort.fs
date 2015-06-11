3f5299a1136ae8af9fb3d4fb29cbc9acb6abcf86
Rui Gonçalves <ruippeixotog@gmail.com>
2015-06-11 19:04:46 +0200

    Add F# implementation

---
+++ f#/quicksort.fs
@@ -0,0 +1,11 @@
+let rec quicksort xs =
+  match xs with
+  | [] -> []
+  | pivot :: tail ->
+      let lower, upper = List.partition (fun x -> x < pivot) tail
+      quicksort lower @ [pivot] @ quicksort upper
+
+let _ = stdin.ReadLine()
+let xs = stdin.ReadLine().Split(' ') |> List.ofArray |> List.map int
+let sorted = quicksort xs
+printfn "%s" (sorted |> List.map (sprintf "%i") |> String.concat " ")
