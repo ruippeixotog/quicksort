3b3f04997f4b656b80fadde2911c12ce7ce80e07
Rui Gonçalves <ruippeixotog@gmail.com>
2015-06-16 23:02:18 +0100

    Add Common Lisp implementation

---
+++ lisp/quicksort.lisp
@@ -0,0 +1,19 @@
+(defun partition (f xs)
+  (labels ((aux (p1 p2 ys)
+                (if (null ys) (cons p1 p2)
+                    (destructuring-bind
+                      (head . tail) ys
+                      (if (funcall f head)
+                          (aux (cons head p1) p2 tail)
+                          (aux p1 (cons head p2) tail))))))
+          (aux '() '() xs)))
+
+(defun quicksort (xs)
+  (if (null xs) xs
+      (let* ((pivot (car xs))
+             (part (partition (lambda (x) (< x pivot)) (cdr xs))))
+        (append (quicksort (car part)) (list pivot) (quicksort (cdr part))))))
+
+(defvar n (read))
+(defvar arr (loop for i below n collect (read)))
+(princ (format nil "~{~A~^ ~}~%" (quicksort arr)))
