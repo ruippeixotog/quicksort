ad05d171958b31fdcd55a9670e6feef315fa2460
Joao Azevedo <joao.c.azevedo@gmail.com>
2015-06-17 03:36:16 +0100

    Return multiple values in partition method

--- lisp/quicksort.lisp
+++ lisp/quicksort.lisp
@@ -2,13 +2,13 @@
   (loop for x in xs
         if (funcall f x) collect x into l
         else collect x into r
-        finally (return (cons l r))))
+        finally (return (values l r))))
 
 (defun quicksort (xs)
   (if (null xs) xs
-      (let* ((pivot (car xs))
-             (part (partition (lambda (x) (< x pivot)) (cdr xs))))
-        (append (quicksort (car part)) (list pivot) (quicksort (cdr part))))))
+    (let ((pivot (car xs)))
+      (multiple-value-bind (l r) (partition (lambda (x) (< x pivot)) (cdr xs))
+        (append (quicksort l) (list pivot) (quicksort r))))))
 
 (defvar n (read))
 (defvar xs (loop for i below n collect (read)))
