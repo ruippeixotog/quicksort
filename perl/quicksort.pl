59af2db24a2aea179155cbe422900df8ba3d45f6
Rui Gonçalves <ruippeixotog@gmail.com>
2015-07-09 01:29:15 +0100

    Add Perl implementation

---
+++ perl/quicksort.pl
@@ -0,0 +1,17 @@
+use strict;
+use warnings;
+
+sub quicksort {
+  return @_ if @_ <= 1;
+
+  my (@lower, @upper);
+  foreach my $e (@_[1..$#_]) {
+    push $e < $_[0] ? \@lower : \@upper, $e;
+  }
+
+  quicksort(@lower), $_[0], quicksort(@upper);
+}
+
+<STDIN>;
+my @arr = split /\s+/, <STDIN>;
+print join(' ', quicksort(@arr)) . "\n";
