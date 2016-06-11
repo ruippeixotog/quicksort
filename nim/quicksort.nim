5326b27f1175ef18d58313a1893b2b47240c3031
Rui Gonçalves <ruippeixotog@gmail.com>
2016-06-11 17:19:06 +0100

    Improve Nim code style

--- nim/quicksort.nim
+++ nim/quicksort.nim
@@ -1,19 +1,19 @@
 import sequtils, strutils
 
-proc quicksort(arr: var openArray[int], st: int, en: int): void =
+proc quicksort(arr: var openArray[int], st: int, en: int) =
   if st == en: return
 
   var sep = st
   for i in countup(st + 1, en - 1):
     if arr[i] < arr[st]:
-      sep += 1
+      inc(sep)
       swap(arr[sep], arr[i])
 
   swap(arr[st], arr[sep])
   quicksort(arr, st, sep)
   quicksort(arr, sep + 1, en)
 
-discard readLine(stdin)
-var arr = map(split(readLine(stdin), " "), parseInt)
+discard stdin.readLine
+var arr = stdin.readLine.split(" ").map(parseInt)
 quicksort(arr, 0, arr.len)
-echo join(arr, " ")
+echo arr.join(" ")
